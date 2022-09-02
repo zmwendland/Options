b2ed0e289aa55fa3e7ffbfdc8c1e9a80c81bde33
Zach Wendland <98436507+zmwendland@users.noreply.github.com>
2022-09-02 03:46:52 -0500

    Update Options.py

--- Options.py
+++ Options.py
@@ -1,73 +1,95 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Aug 14 15:33:01 2022
-
-@author: Zach
-"""
-
 from optionprice import Option as op
 from datetime import datetime as dt
 import pandas as pd
-import numpy as np
+from datetime import datetime, timezone, timedelta
+from yahoo_fin import stock_info as si
 import pandas_datareader as web
 from yahoofinancials import YahooFinancials as yho
+from time import sleep
 
-
-stock = ['OXY'] #<--- ENTER TICKER
+""" Inputs """
+strike = 72.5       #<----- ENTER STRIKE  
+stock = 'uso'       #<--- ENTER TICKER
+opt_type = 'call'   #<--- ENTER 'call' or 'put'
+pp = .37            #<------- ENTER PURCHASE PRICE PER CONTRACT
+contracts1 = 1      #<--- ENTER NUMBER OF CONTRACTS
+expiry_date = dt(2022,9,2)   #<------ ENTER EXPIRATION DATE
+volatility = .9018  #<--- ENTER VOL INDEX/IV OF EXPIRY
+""" """
 
 yf = yho(stock)
-curr_prices = yf.get_current_price()
-my_dict = curr_prices
-my_list = list(my_dict.values())
-current_price = my_list[0]
 
-start = dt(2022,1,1)
+if si.get_market_status() == 'REGULAR':
+    curr_prices22 = si.get_live_price(stock)
 
+elif si.get_market_status != 'REGULAR':
+    curr_prices22 = si.get_premarket_price(stock)
+
+purchases = pp*100
+start = datetime(2022,1,1)
+timezone_offset = -10.0
+tzinfo = timezone(timedelta(hours=timezone_offset))
+    
 price = []
- 
+    
 for s in stock:
     r = web.DataReader(s,'yahoo',start)
     r['stock'] = s
     price.append(r)
-    
-df = pd.concat(price)  
-df = df.reset_index()
-df = df[['Date','stock','Close']]
 
-mean = np.mean(df['Close'])
+rfr = ['^TNX']
+yf2 = yho(rfr)
+curr_rfr = yf2.get_current_price()
 
-dev = []
-
-for p in df['Close']:
-    devs = p - mean
-    dev.append(devs)
-
-sqrt = np.square([x for x in dev])
-sum_sqrt = np.sum([x for x in sqrt])
-var = sum_sqrt/df['Close'].count()
-vol_stdev = np.sqrt(var)
-
-#Risk-free rate (10yr Treasury)
-tbond = ['^TNX']
-yf2 = yho(tbond)
-t_bond_pr = yf2.get_current_price()
-my_dict2 = t_bond_pr
+my_dict2 = curr_rfr
 my_list2 = list(my_dict2.values())
-t_bond_current_price = my_list2[0]
+current_rfr= my_list2[0]/100
 
-      
-#OPTION = manipulate s0, start, and maybe sigma to see price in future
 some_opt = op(european=False,
-                  kind = 'call',#<--- CHOOSE CALL OR PUT (lowercase)
-                  s0=current_price,
-                  k=65, #<--- ENTER STRIKE PRICE
-                  sigma = .4913, #<--- VOL INDEX/IV
-                  r = t_bond_current_price/100, #<--- 10 YEAR T-BILL RATE
-                  start = dt(2022,8,14),
-                  end = dt(2022,8,19),
-                  dv=0)
-price2 = some_opt.getPrice()
-print(round(price2,2))
+                    kind = opt_type,
+                    s0 = curr_prices22,
+                    k = strike,
+                    sigma = volatility, 
+                    r = current_rfr, #<--- 10 YEAR T-BILL RATE
+                    start = dt.now(tzinfo),
+                    end = expiry_date,
+                    dv=0)
 
+price21 = some_opt.getPrice(method='BSM',iteration=5000000)*100
+gain = round(price21-purchases,2)*contracts1
+while True:
+    print('')
+    print('Stock Ticker: ', stock.upper())
+    print('')
+    print('Time: ', si.get_market_status()," MARKET HOURS")
+    print('')
+    print('Current Stock Price:',"${:.2f}".format(curr_prices22))
+    sleep(2)
+    print('')
+    print('Current Option Price:',"${:.2f}".format(round(price21,2)))
+    print('')
+    sleep(2)
+    print('Gain/Loss:',"${:.2f}".format(round(gain,2)))
+    print('')
+    sleep(2)
+    print('% G/L: ','{:.1%}'.format(price21/purchases-1))
+    print('')
+    sleep(2)
+    print('---------------------------------------')
+    sleep(2)
+    if si.get_market_status != 'REGULAR':
+        break
+    else:
+        sleep(10)
+        continue
+    
+    
+        
+        
+        
+    
+    
+
+        
 
 
